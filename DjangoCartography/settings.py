09fba995f8c8295b9ece3f89a9d8ab779057620f
Thibaut <thibautstachnick@gmail.com>
2024-10-22 13:08:04 +0200

    add a map with pointers and direction

--- DjangoCartography/settings.py
+++ DjangoCartography/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'DjangoCartography.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangocartographyproject',
+        'USER': 'cartographyuser',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
