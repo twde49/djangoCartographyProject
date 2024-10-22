fe734b736788941a03f4e83fd6c8efc5f3a48aa3
Thibaut <thibautstachnick@gmail.com>
2024-10-22 19:05:40 +0200

    add a docker file and configuration for the app

--- DjangoCartography/wsgi.py
+++ DjangoCartography/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for DjangoCartography project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+activate_this = "/app/.venv/bin/activate_this.py"
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoCartography.settings")
+
+sys.path.append("/app/DjangoCartography")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoCartography.settings')
 
 application = get_wsgi_application()
